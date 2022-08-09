3d628bf1159a1ad9060a7667465c11113907f910
Kareem Skinner <kareem@karsteski.com>
2022-08-08 22:39:47 -0400

    Added the recommended keybinds to lspconfig

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -1,9 +1,23 @@
-require('lspconfig').pyright.setup({})
+-- In the interest of keeping keymaps in one place.
+require('user.keymaps')
 
-require('lspconfig').clangd.setup({})
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+require('lspconfig')['pyright'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig')['clangd'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
 
 require'lspconfig'.sumneko_lua.setup({
   settings = {
+      on_attach = on_attach,
+      flags = lsp_flags,
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
