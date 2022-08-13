a3f03b52c88bc174382f295d1a770b63e3f31d7a
Kareem Skinner <kareem@karsteski.com>
2022-08-12 23:05:00 -0400

    Moved LSP setup stuff

--- lua/user/plugins/completion.lua
+++ lua/user/plugins/completion.lua
@@ -6,59 +6,6 @@ if not cmp_status_ok then
     return
 end
 
-local lsp_defaults = {
-    flags = {
-        debounce_text_changes = 150
-    },
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    on_attach = ON_ATTACH
-}
-
--- Extend lspconfig's global config
-local lspconfig = require('lspconfig')
-
--- Need to merge lsp_defaults w/ lspconfig's global config. This is the safe way.
-lspconfig.util.default_config = vim.tbl_deep_extend(
-    'force',
-    lspconfig.util.default_config,
-    lsp_defaults
-)
-
-lspconfig['clangd'].setup({
-    single_file_support = true,
-    on_attach = lsp_defaults.on_attach,
-    settings = {
-        lsp_defaults
-    }
-})
-
-lspconfig['sumneko_lua'].setup({
-    single_file_support = true,
-    -- Must call the on_attach in default_config as these options override the global config
-    on_attach = lsp_defaults.on_attach,
-    settings = {
-        lsp_defaults,
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-})
-
 -- Dianostic windows with borders -----------------------------------------
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
   vim.lsp.handlers.hover,
