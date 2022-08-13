14a3d397c58bda6b7f41b02dde82a6252a2152c8
Kareem Skinner <kareem@karsteski.com>
2022-08-12 23:30:47 -0400

    Suppressed annoying message about multiple buffers and different offset_encodings

--- lua/user/plugins/nvim-notify.lua
+++ lua/user/plugins/nvim-notify.lua
@@ -6,3 +6,11 @@ vim.notify = require('notify')
 require('notify').setup({
     background_colour = "#000000"
 })
+
+vim.notify = function(msg, ...)
+    if msg:match("warning: multiple different client offset_encodings") then
+        return
+    end
+
+    vim.notify(msg, ...)
+end
