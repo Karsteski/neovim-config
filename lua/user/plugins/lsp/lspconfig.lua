1a60688d789597ba880637bffcd6ca53f4e2a2f5
Kareem Skinner <kareem@karsteski.com>
2025-09-08 16:27:46 -0400

    Fixed broken on_attach keymaps

--- lua/user/plugins/lsp/lspconfig.lua
+++ lua/user/plugins/lsp/lspconfig.lua
@@ -1,8 +1,25 @@
 -- In the interest of keeping keymaps in one place.
 require("user.keymaps")
 
+local on_attach = ON_ATTACH
+
+-- Make on_attach keymaps work! :)
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("LSP_attach_config", { clear = true }),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        local bufnr = args.buf
+        on_attach(client, bufnr)
+    end
+})
+
+-- To let the LSP know that the global "vim" exists
 vim.lsp.config("lua_ls", {
     settings = {
         Lua = {
             diagnostics = {
-                globals = { "vim" }}}}})
+                globals = { "vim" }
+            }
+        }
+    }
+})
