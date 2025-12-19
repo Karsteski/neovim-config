0066f3c49406a23c1387a729fad75b590fd4cceb
Kareem Skinner <Kareem.Skinner@christiedigital.com>
2025-12-19 13:37:39 -0500

    Fixed nvim-notify timeout

--- lua/user/plugins/nvim-notify.lua
+++ lua/user/plugins/nvim-notify.lua
@@ -3,9 +3,10 @@ if not status_ok then
 	return
 end
 
-notify.setup({
-    timeout = 2000, -- milliseconds
-})
+max_timeout = 1.5
 
 -- Allow other plugins to use the notification windows
-vim.notify = notify
+vim.notify = function (msg, level, opts)
+    opts = opts
+    opts.timeout = math.max(opts.timeout or max_timeout, max_timeout)
+    notify.notify(msg, level, opts)end
